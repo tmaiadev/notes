6f6f23cd8ff81c9247ef4a051c99fcbd1e7826f9
Thalles Maia <tmaiadev@gmail.com>
2019-07-08 21:03:20 -0300

    Clean slate

--- src/components/toolbarSpacer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './toolbarSpacer.css';
-
-export default (props) => {
-    return <div aria-hidden
-                className="toolbar-spacer"
-                tabIndex="-1"></div>
-}
