75cf2be8671824840f9a748de3a8d8dbf116895e
Thalles Maia <tmaiadev@gmail.com>
2019-07-17 14:16:24 +0000

    Hide outline when not using Tab

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginPage from '../login-page/login-page';
 import MainPage from '../main-page/main-page';
 import ActivityIndicator from '../activity-indicator/activity-indicator';
@@ -7,8 +7,32 @@ import './app.css';
 function App() {
   const [activityIndicator, setActivityIndicator] = useState(false);
   const [user, setUser] = useState(null);
+  const [tabUse, setTabUse] = useState(false);
+
+  useEffect(() => {
+    function isUsingTab(evt) {
+      if (evt.key !== 'Tab') return;
+      if (tabUse) return;
+      setTabUse(true);
+    }
+
+    function isNotUsingTab() {
+      if (tabUse === false) return;
+      setTabUse(false);
+    }
+
+    window.addEventListener('keydown', isUsingTab);
+    window.addEventListener('touchstart', isNotUsingTab);
+    window.addEventListener('mousemove', isNotUsingTab);
+    return () => {
+      window.removeEventListener('keydown', isUsingTab);
+      window.removeEventListener('touchstart', isNotUsingTab);
+      window.removeEventListener('mousemove', isNotUsingTab);
+    };
+  }, [tabUse]);
+
   return (
-    <div className="app">
+    <div className={`app ${tabUse === false ? 'app--no-outline' : ''}`}>
       {activityIndicator ? <ActivityIndicator /> : null}
       {user
         ? (
